10a72f73680df5d2f947ae5a1b8324b29829dcc7
Nicholas LaJoie <nlajoie@medrhythms.com>
2021-12-01 09:07:06 -0500

    Success! Day 1 part 1 correct.

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,18 +1,23 @@
 // Author: Nicholas LaJoie
 // Advent of Code, Day 1
 
+use itertools::Itertools;
+
 fn main() -> anyhow::Result<()> {
-    let answer = increases(
-        include_str!("input")
-            .trim_end() // this guy wasted 20 minutes...
-            .split('\n')
-            .map(str::parse::<i64>)
-            .collect::<Result<Vec<_>, _>>()?,
-    );
+    let values = include_str!("input")
+        .trim_end() // this guy wasted 20 minutes...
+        .split('\n')
+        .map(str::parse::<i64>)
+        .collect::<Result<Vec<_>, _>>()?;
+
+    let mut answer = 0;
+    for a in values.windows(2) {
+        if a[1] > a[0] {
+            answer = answer + 1;
+        }
+    }
+
+    dbg!(answer);
 
     Ok(())
-}
-
-fn increases(s: Vec<i64>) -> Option<i64> {
-    todo!()
 }
