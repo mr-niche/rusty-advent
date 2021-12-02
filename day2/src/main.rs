d1c3abb812cb8038e5ab55cfd54a2da42cc38a05
Nicholas LaJoie <nlajoie@medrhythms.com>
2021-12-02 07:14:45 -0500

    Part 2 complete, easy peasy

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -8,16 +8,20 @@ fn main() -> anyhow::Result<()> {
 
     let mut hp = 0;
     let mut dep = 0;
+    let mut aim = 0;
 
     for p in p1_values {
         let mut a = p.split_whitespace().tuples::<(_, _)>();
         let (dir, val) = a.next().unwrap();
         if dir == "forward" {
             hp = hp + val.parse::<i64>().unwrap();
+            dep = dep + (aim * val.parse::<i64>().unwrap());
         } else if dir == "up" {
-            dep = dep - val.parse::<i64>().unwrap();
+            //dep = dep - val.parse::<i64>().unwrap();
+            aim = aim - val.parse::<i64>().unwrap();
         } else if dir == "down" {
-            dep = dep + val.parse::<i64>().unwrap();
+            //dep = dep + val.parse::<i64>().unwrap();
+            aim = aim + val.parse::<i64>().unwrap();
         }
     }
 
