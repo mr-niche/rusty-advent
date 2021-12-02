1bdb1c20487c0a9d66104aaa9cf11ef9a5a228fa
Nicholas LaJoie <nlajoie@medrhythms.com>
2021-12-02 01:50:32 -0500

    Day 2 part 1 complete

---
+++ day2/src/main.rs
@@ -0,0 +1,27 @@
+// Author: Nicholas LaJoie
+// Advent of Code, Day 2
+
+use itertools::Itertools;
+
+fn main() -> anyhow::Result<()> {
+    let p1_values = include_str!("input").trim_end().split('\n');
+
+    let mut hp = 0;
+    let mut dep = 0;
+
+    for p in p1_values {
+        let mut a = p.split_whitespace().tuples::<(_, _)>();
+        let (dir, val) = a.next().unwrap();
+        if dir == "forward" {
+            hp = hp + val.parse::<i64>().unwrap();
+        } else if dir == "up" {
+            dep = dep - val.parse::<i64>().unwrap();
+        } else if dir == "down" {
+            dep = dep + val.parse::<i64>().unwrap();
+        }
+    }
+
+    println!("hp: {}, dp: {}, result: {}", hp, dep, hp * dep);
+
+    Ok(())
+}
