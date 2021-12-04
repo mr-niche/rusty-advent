7f09a7a2dde0c013af7568f75bc4c1e938c04592
Nicholas LaJoie <nlajoie@medrhythms.com>
2021-12-04 15:28:00 -0500

    Getting there, added some printing but need to figure out a shared reference issue so that I can actually update the boards

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -28,9 +28,28 @@ impl BingoCard {
         let bc = Vec::new();
         BingoCard { bingo_card: bc }
     }
-    fn push(&mut self, bn: BingoNumber) -> &Self {
-        self.bingo_card.push(bn);
-        self
+    fn push(&mut self, bn: BingoNumber) {
+        self.bingo_card.push(bn)
+    }
+    fn play(&mut self, called: u8) {
+        for num in &self.bingo_card {
+            if num.number == called {
+                println!("Found {}", called);
+                return;
+            }
+        }
+    }
+    fn print(&self) {
+        let mut y = 0;
+        for num in &self.bingo_card {
+            if y < 4 {
+                print!("{} ", num.number);
+                y = y + 1;
+            } else if y == 4 {
+                println!("{}", num.number);
+                y = 0;
+            }
+        }
     }
 }
 
@@ -59,9 +78,11 @@ fn main() -> anyhow::Result<()> {
     let _input: Vec<&str> = include_str!("input").trim_end().split("\n\n").collect();
 
     // Parse input for call_sequence, BingoCards
-    let _bg = parse_input(example);
+    let bg: Option<BingoGame> = parse_input(example);
+    //dbg!(bg);
 
-    //dbg!(example);
+    // Now we have a bingo game, let's go through the call numbers!
+    let winner = play_bingo(&bg.unwrap());
 
     Ok(())
 }
@@ -98,9 +119,21 @@ fn parse_input(input: Vec<&str>) -> Option<BingoGame> {
             y = y + 1;
             x = 0;
         }
+        bg.bingo_cards.push(bc);
     }
-
     Some(bg)
+}
+
+fn play_bingo(bg: &BingoGame) -> Option<BingoCard> {
+    for num in &bg.call_sequence {
+        println!("Calling...{}!", num);
+        for card in &bg.bingo_cards {
+            card.print();
+            println!();
+            //dbg!(board);
+        }
+    }
+    None
 }
 
 // TESTS!
