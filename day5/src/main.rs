9c9a22b36613ee5420ea4cde6887bf7c4c01d7f5
Nicholas LaJoie <nlajoie@medrhythms.com>
2021-12-05 10:46:50 -0500

    Printing done, getting there

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -7,6 +7,15 @@ struct Vent {
     b: Point,
 }
 
+impl Vent {
+    fn is_flat(&self) -> bool {
+        if self.a.x == self.b.x || self.a.y == self.b.y {
+            return true;
+        }
+        false
+    }
+}
+
 #[derive(Debug, Clone, Copy, PartialEq, Default)]
 struct Point {
     x: usize,
@@ -19,11 +28,44 @@ impl From<(usize, usize)> for Point {
     }
 }
 
+#[derive(PartialEq, Debug, Default)]
+struct OceanField {
+    origin: Point,
+    extent: Point,
+    field: Vec<usize>,
+}
+
+impl OceanField {
+    fn new() -> OceanField {
+        let of = Vec::new();
+        OceanField {
+            origin: Point::default(),
+            extent: Point::default(),
+            field: of,
+        }
+    }
+    fn print(&self) {
+        for i in 0..self.extent.y {
+            for j in 0..self.extent.x {
+                let val = self.field.iter().next().unwrap();
+                if *val == 0 {
+                    print!(".");
+                } else {
+                    println!("{}", val);
+                }
+            }
+            println!();
+        }
+    }
+}
+
 fn main() -> anyhow::Result<()> {
     let example: Vec<&str> = include_str!("example").trim_end().split('\n').collect();
     let _input: Vec<&str> = include_str!("input").trim_end().split('\n').collect();
 
     let mut vents: Vec<Vent> = Vec::new();
+    let mut big_x: usize = 0;
+    let mut big_y: usize = 0;
     for line in example {
         let vent = line.split(" -> ").collect::<Vec<&str>>();
         // Get Point a
@@ -45,11 +87,55 @@ fn main() -> anyhow::Result<()> {
             a: Point { x: a[0], y: a[1] },
             b: Point { x: b[0], y: b[1] },
         });
+
+        // Update our field height/widths
+        if a[0] > big_x {
+            big_x = a[0];
+        }
+        if b[0] > big_x {
+            big_x = b[0];
+        }
+        if a[1] > big_y {
+            big_y = a[1];
+        }
+        if b[1] > big_y {
+            big_y = b[1];
+        }
     }
-    dbg!(vents);
+    //dbg!(vents);
+
+    // Create an Ocean Field, and fill it up with 0s
+    let mut ocean_field = OceanField::default();
+    ocean_field.origin = (0, 0).into();
+    ocean_field.extent = (big_x, big_y).into();
+
+    for i in 0..big_y {
+        for j in 0..big_x {
+            ocean_field.field.push(0);
+        }
+    }
+    ocean_field.print();
+
+    // Now, iterate through our vents (the flat ones), and update the field
 
     Ok(())
 }
 
 #[test]
-fn test_vent() {}
+fn test_vent_is_flat() {
+    let vertical = Vent {
+        a: (1, 1).into(),
+        b: (1, 3).into(),
+    };
+    let horizontal = Vent {
+        a: (1, 1).into(),
+        b: (3, 1).into(),
+    };
+    let diagonal = Vent {
+        a: (1, 1).into(),
+        b: (3, 5).into(),
+    };
+    assert_eq!(vertical.is_flat(), true);
+    assert_eq!(horizontal.is_flat(), true);
+    assert_eq!(diagonal.is_flat(), false);
+}
