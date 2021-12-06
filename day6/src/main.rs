d5143afcec248323476a05f056fc7d5ffd1e5e31
Nicholas LaJoie <nlajoie@medrhythms.com>
2021-12-06 13:14:38 -0500

    Starting over

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -17,7 +17,7 @@ fn main() -> anyhow::Result<()> {
         .collect();
 
     let mut school_of_fish: Vec<usize> = Vec::new();
-    for age in input {
+    for age in example {
         school_of_fish.push(age);
     }
 
@@ -28,19 +28,37 @@ fn main() -> anyhow::Result<()> {
     }
     println!();
 
-    // Start our total with our inital # of fish
-    let mut total = school_of_fish.len();
-    println!("Start with {} fish...", total);
-    println!();
-    for fish in school_of_fish {
-        println!("Init: {}", fish);
-        let this_fish = future_fish(fish, 80 + 1);
-        total = total + this_fish;
-        println!("fish produced {}", this_fish);
-        println!();
+    // How many days will be calculate for?
+    let max_day = 80;
+
+    // Pre-calculate future_fish(8, days) values
+    let mut fish_days: Vec<isize> = Vec::new();
+    for day in 1..max_day + 1 {
+        let calc = (day - 2) / 7;
+        if day < 10 {
+            fish_days.push(0);
+        } else {
+            fish_days.push(calc);
+        }
     }
 
-    println!("...end with: {} !", total);
+    for (i, f) in fish_days.iter().enumerate() {
+        println!("future_fish(8, {}) = {}", i + 1, f);
+    }
+
+    // Start our total with our inital # of fish
+    //let mut total = school_of_fish.len();
+    //println!("Start with {} fish...", total);
+    //println!();
+    //for fish in school_of_fish {
+    //    println!("Init: {}", fish);
+    //    let this_fish = future_fish(fish, 80);
+    //    total = total + this_fish;
+    //    println!("fish produced {}", this_fish);
+    //    println!();
+    //}
+
+    //println!("...end with: {} !", total);
 
     Ok(())
 }
@@ -48,24 +66,26 @@ fn main() -> anyhow::Result<()> {
 fn future_fish(mut age: usize, mut days: usize) -> usize {
     let mut acc = 0;
 
-    //println!("future_fish({}, {})", age, days);
-    if days >= age {
-        let mut num = 0;
+    let init_num = (days + (6 - age)) / 7;
 
-        if age == 8 {
-            num = (days - 3) / 7;
-        } else {
-            num = (days + (5 - age)) / 7;
-        }
+    println!(
+        "future_fish({}, {}) will produce {} fish",
+        age, days, init_num
+    );
+    acc = init_num;
 
-        acc = acc + num;
-
-        // But how many will _those_ fish produce??
-        for i in 0..num {
-            let remaining = days - (age + 1) - (7 * i);
-            if remaining > 0 {
-                acc = acc + future_fish(8, remaining);
+    // Calculate the rest
+    for i in 0..init_num {
+        let mut remaining = days - (age + 1) - (7 * i);
+        if remaining > 7 {
+            let mut v = 0;
+            let mut d = remaining;
+            while d > 8 {
+                v = v + (d - 3) / 7;
+                d = d - 7;
             }
+            acc = acc + v;
+            println!("future_fish(8, {}) = {}", remaining, v);
         }
     }
     acc
