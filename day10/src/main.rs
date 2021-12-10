b9756fc8ffaa6ea85eb306f3144deff881879a0b
Nicholas LaJoie <nlajoie@medrhythms.com>
2021-12-10 17:20:18 -0500

    Part 2 completo, pretty easy thanks to kento

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,29 +1,30 @@
 // Author: Nicholas LaJoie
+// Co-Auther: Kent Seneres (the man with the wisdom)
 // Advent of Code, Day 10
 
 fn main() -> anyhow::Result<()> {
     let _example: Vec<&str> = include_str!("example").trim_end().split('\n').collect();
     let input: Vec<&str> = include_str!("input").trim_end().split('\n').collect();
 
-    let mut acc = 0;
+    let mut scores: Vec<usize> = Vec::new();
     for line in input {
         let l = String::from(line);
-        let result = process_line(&l);
-        if result.is_some() {
-            match result.unwrap() {
-                ')' => acc = acc + 3,
-                ']' => acc = acc + 57,
-                '}' => acc = acc + 1197,
-                '>' => acc = acc + 25137,
-                _ => continue,
-            }
+        let result = get_bad_char(&l);
+        if result.is_none() {
+            //println!("{} is incomplete", l);
+            scores.push(autocomplete(&l));
         }
     }
-    dbg!(acc);
+
+    // Find the middle score
+    scores.sort();
+    let winner = scores[scores.len() / 2];
+    dbg!(winner);
+
     Ok(())
 }
 
-fn process_line(line: &String) -> Option<char> {
+fn get_bad_char(line: &String) -> Option<char> {
     let mut list: Vec<char> = Vec::new();
     for l in line.chars() {
         if is_opener(&l) {
@@ -35,16 +36,39 @@ fn process_line(line: &String) -> Option<char> {
                 // We found a pair!
                 list.pop();
             } else {
-                println!(
-                    "Expected {}, but found {} instead",
-                    get_closer(&last).unwrap(),
-                    l
-                );
                 return Some(l);
             }
         }
     }
     None
+}
+
+fn autocomplete(line: &String) -> usize {
+    let mut list: Vec<char> = Vec::new();
+    for l in line.chars() {
+        if is_opener(&l) {
+            list.push(l);
+        } else if is_closer(&l) {
+            let last = &list.last().unwrap();
+            let l_opener = get_opener(&l).unwrap();
+            if **last == l_opener {
+                // We found a pair!
+                list.pop();
+            }
+        }
+    }
+    // Get our score
+    let mut acc = 0;
+    for l in list.into_iter().rev() {
+        match l {
+            '(' => acc = (acc * 5) + 1,
+            '[' => acc = (acc * 5) + 2,
+            '{' => acc = (acc * 5) + 3,
+            '<' => acc = (acc * 5) + 4,
+            _ => continue,
+        }
+    }
+    acc
 }
 
 fn is_opener(character: &char) -> bool {
