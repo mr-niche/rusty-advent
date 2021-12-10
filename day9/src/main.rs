4672f396d13749684ddace471ee0b29cf7f872f3
Nicholas LaJoie <nlajoie@medrhythms.com>
2021-12-09 21:05:14 -0500

    This is wayyyy better for part one, more readable and clean? And hopefully more helpful for part 2?

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -2,15 +2,17 @@
 // Advent of Code, Day 9
 
 fn main() -> anyhow::Result<()> {
-    let example: Vec<&str> = include_str!("example").trim_end().split('\n').collect();
+    let _example: Vec<&str> = include_str!("example").trim_end().split('\n').collect();
+    let input: Vec<&str> = include_str!("input").trim_end().split('\n').collect();
 
     // Let's make a 2D Vec
     let mut field: Vec<Vec<u32>> = Vec::new();
-    for nums in example {
+    for nums in input {
         field.push(nums.chars().map(|d| d.to_digit(10).unwrap()).collect());
     }
 
     let result = get_lows(&field);
+    dbg!(result);
 
     Ok(())
 }
@@ -20,45 +22,28 @@ fn get_lows(field: &Vec<Vec<u32>>) -> u32 {
     let mut acc = 0;
     for (row, val) in field.iter().enumerate() {
         for (col, digit) in val.iter().enumerate() {
-            let mut is_low = true;
-            println!("{}:{} = {}", row, col, digit);
+            let mut neighbors: Vec<u32> = Vec::new();
             // Go left
             if !(col == 0) {
-                println!(
-                    "  Left: Check {}:{} = {}",
-                    row,
-                    col - 1,
-                    val.get(col - 1).unwrap()
-                );
+                neighbors.push(*val.get(col - 1).unwrap());
             }
             // Go up
             if !(row == 0) {
-                println!(
-                    "  Up:   Check {}:{} = {}",
-                    row - 1,
-                    col,
-                    field[row - 1].get(col).unwrap()
-                );
+                neighbors.push(*field[row - 1].get(col).unwrap());
             }
             // Go right
             if !(col == val.len() - 1) {
-                println!(
-                    "  Right: Check {}:{} = {}",
-                    row,
-                    col + 1,
-                    val.get(col + 1).unwrap()
-                );
+                neighbors.push(*val.get(col + 1).unwrap());
             }
             // Go down
             if !(row == field.len() - 1) {
-                println!(
-                    "  Down: Check {}:{} = {}",
-                    row + 1,
-                    col,
-                    field[row + 1].get(col).unwrap()
-                );
+                neighbors.push(*field[row + 1].get(col).unwrap());
+            }
+            // Check it
+            if digit < neighbors.iter().min().unwrap() {
+                acc = acc + digit + 1;
             }
         }
     }
-    0
+    acc
 }
