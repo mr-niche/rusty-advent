1ce00749e30aced72f8bfc4e1f5b8e8263c8f687
Nicholas LaJoie <nlajoie@medrhythms.com>
2021-12-11 11:27:26 -0500

    We're stepping and flashing and printing and it looks mighty cool. No ripple effect yet though, that's coming next

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -32,14 +32,41 @@ impl Consortium {
         }
         None
     }
+    fn clear_all(&mut self) {
+        for octopus in &mut self.octopuses {
+            octopus.clear();
+        }
+    }
+    fn step_all(&mut self) {
+        for octopus in &mut self.octopuses {
+            octopus.step();
+        }
+    }
 }
 
-#[derive(Debug, Clone, PartialEq, Default)]
+#[derive(Debug, Clone, Copy, PartialEq, Default)]
 struct Octopus {
     location: Point,
     energy_level: u32,
     flash_count: u32,
     flashed: bool,
+}
+
+impl Octopus {
+    fn clear(&mut self) {
+        self.flashed = false;
+    }
+    fn step(&mut self) {
+        match self.energy_level {
+            // TODO: Will need to validate it hasn't flashed already
+            9 => {
+                self.energy_level = 0;
+                self.flash_count = self.flash_count + 1;
+                self.flashed = true;
+            }
+            _ => self.energy_level = self.energy_level + 1,
+        }
+    }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Default)]
@@ -80,17 +107,114 @@ fn main() -> anyhow::Result<()> {
     }
 
     // Now let's process the steps
-    let steps = 100;
+    //let steps = 100;
+    let steps = 5;
     for step in 0..steps + 1 {
         if step == 0 {
             println!("Before any steps:");
-            consortium.print()
+            consortium.print();
+            println!();
         } else {
-            //println!("After step {}:", step);
-            // consortium.print()
-            continue;
+            // 1. Update all octopus energies
+            consortium.step_all();
+
+            // 2. Do the rippling flashes
+            // 3. Clear the flashes
+            consortium.clear_all();
+
+            // 4. Print
+            println!("After step {}:", step);
+            consortium.print();
+            println!();
         }
     }
 
     Ok(())
+}
+
+#[test]
+fn test_octopus_clear() {
+    let mut octopus = Octopus {
+        location: Point { row: 0, col: 0 },
+        energy_level: 1,
+        flash_count: 0,
+        flashed: true,
+    };
+    assert_eq!(octopus.flashed, true);
+    octopus.clear();
+    assert_eq!(octopus.flashed, false);
+}
+
+#[test]
+fn test_consortium_clear_all() {
+    let mut consortium: Consortium = Consortium {
+        octopuses: Vec::new(),
+        width: 10,
+        height: 10,
+    };
+    consortium.octopuses.push(Octopus {
+        location: Point { row: 0, col: 0 },
+        energy_level: 1,
+        flash_count: 0,
+        flashed: true,
+    });
+    consortium.octopuses.push(Octopus {
+        location: Point { row: 0, col: 1 },
+        energy_level: 2,
+        flash_count: 0,
+        flashed: false,
+    });
+    consortium.octopuses.push(Octopus {
+        location: Point { row: 0, col: 2 },
+        energy_level: 3,
+        flash_count: 0,
+        flashed: true,
+    });
+
+    assert_eq!(
+        consortium
+            .get_octopus(&Point { row: 0, col: 0 })
+            .unwrap()
+            .flashed,
+        true
+    );
+    assert_eq!(
+        consortium
+            .get_octopus(&Point { row: 0, col: 1 })
+            .unwrap()
+            .flashed,
+        false
+    );
+    assert_eq!(
+        consortium
+            .get_octopus(&Point { row: 0, col: 2 })
+            .unwrap()
+            .flashed,
+        true
+    );
+
+    // Clear em
+    consortium.clear_all();
+
+    assert_eq!(
+        consortium
+            .get_octopus(&Point { row: 0, col: 0 })
+            .unwrap()
+            .flashed,
+        false
+    );
+    assert_eq!(
+        consortium
+            .get_octopus(&Point { row: 0, col: 1 })
+            .unwrap()
+            .flashed,
+        false
+    );
+    assert_eq!(
+        consortium
+            .get_octopus(&Point { row: 0, col: 2 })
+            .unwrap()
+            .flashed,
+        false
+    );
 }
